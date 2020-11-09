e2100b09c093e318aae4ced6873bd15ce41bc5dd
BENJAMIN TITUS NYONI <benjie.nyoniak47@gmail.com>
2020-11-09 13:27:23 +0200

    Dimensions Data

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_2__Dimensions_Data.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
