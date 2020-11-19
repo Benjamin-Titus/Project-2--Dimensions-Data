1fd667d4dc9fc72947ebe1a89b404215d1b02f43
BENJAMIN TITUS NYONI <benjie.nyoniak47@gmail.com>
2020-11-19 11:26:57 +0200

    Dimension Data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,24 +1,17 @@
-﻿#pragma warning disable CS0234 // The type or namespace name 'EntityFrameworkCore' does not exist in the namespace 'Microsoft.AspNetCore.Identity' (are you missing an assembly reference?)
-#pragma warning restore CS0234 // The type or namespace name 'EntityFrameworkCore' does not exist in the namespace 'Microsoft.AspNetCore.Identity' (are you missing an assembly reference?)
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Project_2__Dimensions_Data.Data;
-
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Project_2__Dimensions_Data.Data
 {
-#pragma warning disable CS0246 // The type or namespace name 'IdentityDbContext' could not be found (are you missing a using directive or an assembly reference?)
     public class ApplicationDbContext : IdentityDbContext
-#pragma warning restore CS0246 // The type or namespace name 'IdentityDbContext' could not be found (are you missing a using directive or an assembly reference?)
     {
-
-
-#pragma warning disable CS0311 // The type 'Project_2__Dimensions_Data.Data.ApplicationDbContext' cannot be used as type parameter 'TContext' in the generic type or method 'DbContextOptions<TContext>'. There is no implicit reference conversion from 'Project_2__Dimensions_Data.Data.ApplicationDbContext' to 'Microsoft.EntityFrameworkCore.DbContext'.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-#pragma warning restore CS0311 // The type 'Project_2__Dimensions_Data.Data.ApplicationDbContext' cannot be used as type parameter 'TContext' in the generic type or method 'DbContextOptions<TContext>'. There is no implicit reference conversion from 'Project_2__Dimensions_Data.Data.ApplicationDbContext' to 'Microsoft.EntityFrameworkCore.DbContext'.
-            : base(options)
-        {
-
+           : base(options)
+        { 
         }
-
     }
+
 }
--- Startup.cs
+++ Startup.cs
@@ -24,10 +24,15 @@ namespace Project_2__Dimensions_Data
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+           
+
             services.AddControllersWithViews();
             services.AddRazorPages();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
